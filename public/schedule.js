f1c46118c41a55c7f8f431d32d7a3383d4881e82
Dave Geddes <davidcgeddes@gmail.com>
2015-03-04 14:56:18 -0700

    wip

--- public/schedule.js
+++ public/schedule.js
@@ -1,10 +1,44 @@
-var schedule = angular.module('schedule', [])
-  .config(function($sceProvider) {
-    $sceProvider.enabled(false);
-  });;
+var schedule = angular.module('schedule', ['ngRoute']);
+
+schedule.config(function($routeProvider, $sceProvider, $locationProvider) {
+  $sceProvider.enabled(false);
+  
+  $locationProvider.html5Mode(true);
+  
+  console.log("$routeProvider", $routeProvider);
+  
+  $routeProvider.when('/details/3', {
+    controller: 'DetailsCtrl',
+    templateUrl: 'details.html'
+  });
+  
+  // $routeProvider.otherwise({controller: 'ScheduleCtrl', template: 'schedule.html'});
+  
+  
+  // $routeProvider.when('/', { templateUrl: '/schedule.html', controller: 'ScheduleCtrl' })
+  
+    // .when('/details/:session', {
+    //   templateUrl: '/details.html',
+    //   controller: 'DetailsCtrl'
+    // })
+    // 
+    // .otherwise({
+    //   templateUrl: 'add-order.html',
+    //   controller: 'AddOrderController'
+    // })
+    // $routeProvider.when('/', {controller: 'ScheduleCtrl', template: 'schedule.html'});
+    // $routeProvider.when('/details', {controller: 'DetailsCtrl', template: 'details.html'});
+});
+  
+  
 schedule.controller('ScheduleCtrl', function($http) {
+  alert('schedule');
   $http.get('schedule.json')
     .then(function(schedule) {
       this.schedule = schedule.data;
     }.bind(this));
 });
+
+schedule.controller('DetailsCtrl', function(){
+  alert('details');
+})
